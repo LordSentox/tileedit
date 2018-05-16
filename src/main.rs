9c14cc4814d8dfbb1d982975974b41ea77beb578
Arne Dussin <arne.dussin@live.de>
2018-05-16 11:59:53 +0200

    Make TileLayerView draggable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![allow(dead_code)]
+
 extern crate find_folder;
 extern crate gfx_device_gl;
 extern crate graphics;
+extern crate nalgebra;
 extern crate piston_window;
 extern crate sdl2_window;
 extern crate sprite;
@@ -10,11 +13,14 @@ mod texture_manager;
 mod tile_layer_view;
 
 pub type DeviceTexture = Texture<gfx_device_gl::Resources>;
+pub use nalgebra::Vector2 as Vec2;
 
 use piston_window::*;
 use sdl2_window::Sdl2Window;
 
+use ani_sprite::AniSprite;
 use texture_manager::TextureManager;
+use tile_layer_view::TileLayerView;
 
 fn main() {
 	let mut window: PistonWindow<Sdl2Window> = WindowSettings::new("TileEdit", (900, 900))
@@ -26,9 +32,27 @@ fn main() {
 
 	let textures = TextureManager::new(&find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").expect("Could not find asset folder"));
 
+	let mut test_layer_view = TileLayerView::new((5, 5), AniSprite::new(textures.get("tiles.png", &mut window.factory), 64., 64.));
+
+	let tilemap = vec![vec![
+		Some(0), Some(1), Some(0), Some(1), Some(0)
+	]; 5];
+
+	test_layer_view.clone_map(&tilemap);
+
 	while let Some(e) = window.next() {
+		e.mouse_relative(|dx, dy| {
+			test_layer_view.mouse_relative(dx, dy);
+		});
+
+		e.button(|b| {
+			test_layer_view.button(&b);
+		});
+		
 		window.draw_2d(&e, |c, g| {
 			clear([1.0, 1.0, 1.0, 1.0], g);
+
+			test_layer_view.draw(c.transform, g);
 		});
 	}
 }
--- src/tile_layer_view.rs
+++ src/tile_layer_view.rs
@@ -1,32 +1,63 @@
 use ani_sprite::AniSprite;
 use graphics::{Graphics, math::Matrix2d};
+use piston_window::{Button, ButtonArgs, ButtonState, MouseButton};
 use super::DeviceTexture;
 use std::cell::RefCell;
 use std::u16;
+use super::Vec2;
 
 pub type Tilemap = Vec<Vec<Option<u16>>>;
 
 pub struct TileLayerView {
+	pos: Vec2<f64>,
 	tilemap: Tilemap,
-	tiles: RefCell<AniSprite<DeviceTexture>>
+	tiles: RefCell<AniSprite<DeviceTexture>>,
+	grabbed: bool
 }
 
 impl TileLayerView {
+	/// Create a new TileLayerView with the specified size. The tiles are in
+	/// form of an AniSprite.
 	pub fn new((width, height): (usize, usize), tiles: AniSprite<DeviceTexture>) -> TileLayerView {
 		TileLayerView {
+			pos: Vec2::new(0., 0.),
 			tilemap: vec![vec![None; width]; height],
-			tiles: RefCell::new(tiles)
+			tiles: RefCell::new(tiles),
+			grabbed: false
 		}
 	}
 
+	/// Set the tile at position (x, y) to the value of to. Update without
+	/// buffering.
 	pub fn set_tile(&mut self, (x, y): (usize, usize), to: Option<u16>) {
 		self.tilemap[x][y] = to;
 	}
 
+	/// Clone the tilemap information to this map. The individual values have to
+	/// be copied over.
 	pub fn clone_map(&mut self, map: &Tilemap) {
 		self.tilemap = map.clone();
 	}
 
+	/// When the mouse is moved by a relative amount this has to be called to
+	/// keep the map at the correct place and whatnot.
+	pub fn mouse_relative(&mut self, dx: f64, dy: f64) {
+		if self.grabbed {
+			self.pos += Vec2::new(dx, dy);
+		}
+	}
+
+	/// Called when a button is pressed. All shortcuts that are applied to this
+	/// TileLayerView can be handled in this function.
+	pub fn button(&mut self, b: &ButtonArgs) {
+		// Toggle wether or not the map is currently being dragged
+		if let Button::Mouse(MouseButton::Middle) = b.button {
+			self.grabbed = b.state == ButtonState::Press;
+		}
+	}
+
+	/// Draw the TileLayerView to a Texture (unbuffered; every tile is drawn on
+	/// its own, so it's quite slow.
 	pub fn draw<B: Graphics<Texture=DeviceTexture>>(&self, t: Matrix2d, b: &mut B) {
 		let (width, height) = (self.tiles.borrow().frame_width(), self.tiles.borrow().frame_width());
 		let mut tiles = self.tiles.borrow_mut();
@@ -35,7 +66,7 @@ impl TileLayerView {
 			for (y, e) in row.iter().enumerate() {
 					if let Some(e) = *e {
 					tiles.set_frame(e as u32);
-					tiles.set_position(x as f64*width, y as f64*height);
+					tiles.set_position(self.pos.x + x as f64*width, self.pos.y + y as f64*height);
 
 					tiles.draw(t, b);
 				}
